6d00c269598d3563df4fed2837a90b22bbb70512
Carlos Pantelides <carlos.pantelides@gmail.com>
2022-03-01 09:31:22 -0300

    api: added sql db

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {MongoMemoryServer} = require("mongodb-memory-server");
 const {MongoClient} = require("mongodb");
+const PgMem = require("pg-mem");
+
+const db = PgMem.newDb();
+
+
 
 let database = null;
 const collectionName = "measurements";
@@ -41,12 +46,15 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 // Ejercicio: agregar timestamp
 // Ejercicio: validar message
+// Ejercicio: no publicar key en GET device
+// Ejercicio: usar key como autenticación del POST measurement
 // Ejercicio: recibir json
 
 const PORT = 8080;
 
 app.post('/measurement', function (req, res) {
 	console.log("device i    : " + req.body.id );
+	console.log("key         : " + req.body.key );
 	console.log("temperature : " + req.body.t);
 	console.log("humidity    : " + req.body.h);
         const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t, h:req.body.h});
@@ -64,10 +72,20 @@ app.get('/measurement', async (req,res) => {
     res.send(await getMeasurements());
 });
 
+app.get('/device', function(req,res) {
+    res.send( db.public.many("SELECT * FROM devices") );
+});
+
+
 startDatabase().then(async() => {
     await insertMeasurement({id:'1', t:'18', h:'78'});
     await insertMeasurement({id:'1', t:'19', h:'77'});
     await insertMeasurement({id:'2', t:'17', h:'77'});
+
+    db.public.none("CREATE TABLE devices (device_id VARCHAR, key VARCHAR)");
+    db.public.none("INSERT INTO devices VALUES ('1', '123456')");
+    db.public.none("INSERT INTO devices VALUES ('2', '234567')");
+    db.public.none("INSERT INTO devices VALUES ('3', '345678')");
 
     app.listen(PORT, () => {
         console.log(`Listening at ${PORT}`);
---
+++ tools/get_devices.sh
@@ -0,0 +1,2 @@
+wget -O output.txt "http://localhost:8080/device"
+cat output.txt
