0465c4cfdd8e3958d64022167ca1b9ce30430dad
Carlos Pantelides <carlos.pantelides@gmail.com>
2022-03-01 09:31:22 -0300

    api: added sql db

--- api/index.js
+++ api/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {MongoMemoryServer} = require("mongodb-memory-server");
 const {MongoClient} = require("mongodb");
+const PgMem = require("pg-mem");
+
+const db = PgMem.newDb();
 
 let database = null;
 const collectionName = "measurements";
 
 async function startDatabase() {
     const mongod = await MongoMemoryServer.create();
-
     const uri = mongod.getUri();	
-
     const connection = await MongoClient.connect(uri, {useNewUrlParser: true});
-
     database = connection.db();
 }
 
@@ -22,52 +22,102 @@ async function getDatabase() {
 }
 
 async function insertMeasurement(message) {
-    const database = await getDatabase();
     const {insertedId} = await database.collection(collectionName).insertOne(message);
     return insertedId;
 }
 
 async function getMeasurements() {
-    const database = await getDatabase();
     return await database.collection(collectionName).find({}).toArray();	
 }
-
-
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
-//app.use(bodyParser.json());
-
-// Ejercicio: agregar timestamp
-// Ejercicio: validar message
-// Ejercicio: recibir json
 
 const PORT = 8080;
 
 app.post('/measurement', function (req, res) {
 	console.log("device i    : " + req.body.id );
+	console.log("key         : " + req.body.key );
 	console.log("temperature : " + req.body.t);
 	console.log("humidity    : " + req.body.h);
         const {insertedId} = insertMeasurement({id:req.body.id, t:req.body.t, h:req.body.h});
-
 	res.send("received request into " +  insertedId);
 });
 
-app.get('/', function (req, res) {
-	console.log("device i    : " + req.query.id );
-	console.log("temperature : " + req.query.t);
-	console.log("humidity    : " + req.query.h);
-	res.send("received request");
+app.get('/web/device', function (req, res) {
+	var devices = db.public.many("SELECT * FROM devices").map( function(device) {
+		console.log(device);
+		return '<tr><td><a href=/web/device/'+ device.device_id +'>' + device.device_id + "</a>" +"</td><td>"+ device.key+"</td></tr>";
+	   }
+	);
+	res.send("<html>"+
+		     "<head><title>Sensores</title></head>" +
+		     "<body>" +
+		        "<table border=\"1\">" +
+		           "<tr><th>id</th><th>key</th></tr>" +
+		           devices +
+		        "</table>" +
+		     "</body>" +
+		"</html>");
 });
+
+/*
+ * Canibalized from
+ *    https://www.npmjs.com/package/sprightly
+ *    https://github.com/obadakhalili/Sprightly/blob/main/index.js
+ */
+function render(template, vars) {
+   const regexp = /<<(.*?)>>|\{\{(.*?)\}\}/;
+   return template.split('\n').map( function(line) {
+       for (let match = line.match(regexp), result; match;) {
+	   if (match[0][0] === '<') {
+		   console.log("match <");
+	   } else {
+	      result = vars[match[2].trim()];
+
+	   }
+           line = line.replace(match[0], result ? result : '');
+	   match = line.match(regexp);
+       }	       
+       return line;
+   }).join('\n');	
+}
+
+app.get('/web/device/:id', function (req, res) {
+    var template = "<html>"+
+                     "<head><title>Sensor {{id}}</title></head>" +
+                     "<body>" +
+		        "<h1>{{ id }}</h1>"+
+		        "Key : {{ key }}" +
+                     "</body>" +
+                "</html>";
+
+
+    var device = db.public.many("SELECT * FROM devices WHERE device_id = '"+req.params.id+"'");
+    console.log(device);
+    res.send(render(template,{id:device[0].device_id, key: device[0].key}));
+});
+
 app.get('/measurement', async (req,res) => {
     res.send(await getMeasurements());
+});
+
+app.get('/device', function(req,res) {
+    res.send( db.public.many("SELECT * FROM devices") );
 });
 
 startDatabase().then(async() => {
     await insertMeasurement({id:'1', t:'18', h:'78'});
     await insertMeasurement({id:'1', t:'19', h:'77'});
     await insertMeasurement({id:'2', t:'17', h:'77'});
+    console.log("mongo measurement database Up");
+
+    db.public.none("CREATE TABLE devices (device_id VARCHAR, key VARCHAR)");
+    db.public.none("INSERT INTO devices VALUES ('1', '123456')");
+    db.public.none("INSERT INTO devices VALUES ('2', '234567')");
+    db.public.none("INSERT INTO devices VALUES ('3', '345678')");
+    console.log("sql device database up");
 
     app.listen(PORT, () => {
         console.log(`Listening at ${PORT}`);
