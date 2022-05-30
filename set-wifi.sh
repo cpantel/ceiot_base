d201ba5b3efa52c331e459919ea7345a5fa7e048
Marcelo García <garcia.marcelor@gmail.com>
2022-05-30 01:23:35 +0000

    220529 Agregado de / en set-wifi

--- set-wifi.sh
+++ set-wifi.sh
@@ -1,7 +1,7 @@
 SSID=$( grep "^#define WIFI_SSID"  ../config/config.h | rev | cut -d'"' -f 2 | rev )
 PASSWORD=$( grep "^#define WIFI_PASSWORD"  ../config/config.h | rev | cut -d'"' -f 2 | rev )
 
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"' 
+sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"/' 
 sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/' 
 
 
