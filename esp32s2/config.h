c4d0a9de2e20ef366387c530cbaa086a24b32f11
Raúl Emilio Romero <rraulemilioromero@gmail.com>
2022-06-10 14:19:59 +0000

    actuacización

---
+++ esp32s2/config.h
@@ -0,0 +1,13 @@
+#ifndef _config_
+
+#define API_IP        "192.168.0.100"
+#define API_PORT      "8080"
+#define API_IP_PORT   API_IP ":" API_PORT
+
+#define WIFI_SSID     "wCUO"
+#define WIFI_PASSWORD "CUO%2017"
+
+#define DEVICE_ID     11
+
+#define _config_
+#endif
