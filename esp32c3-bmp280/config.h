d17b4584626dcdb1b7123acdbadc6e2db64ccf11
Raúl Emilio Romero <rraulemilioromero@gmail.com>
2022-06-11 11:24:50 +0000

    cambios 11-06

---
+++ esp32c3-bmp280/config.h
@@ -0,0 +1,15 @@
+#ifndef _config_
+
+#define API_IP        "192.168.103.197" // esto cambia porque es ip dinamica
+#define API_PORT      "8080"
+#define API_IP_PORT   API_IP ":" API_PORT
+
+#define WIFI_SSID     "wCUO"
+#define WIFI_PASSWORD "cuo%2017"
+
+#define DEVICE_ID     "11"
+#define SDA_GPIO      4
+#define SCL_GPIO      5
+
+#define _config_
+#endif
