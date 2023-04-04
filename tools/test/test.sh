5c66e6c4426a045d80f45d74d08aba39ee6a7982
Carlos Pantelides <carlos.pantelides@gmail.com>
2023-04-04 12:20:16 -0300

    Ignore non existent devices.json on delete

--- tools/test/test.sh
+++ tools/test/test.sh
@@ -1,5 +1,5 @@
 function testPostDevice() {
-   rm devices.json	
+   rm -f devices.json	
    ../post_device.sh id_03 name_03 key_03  > /dev/null 2>&1
    ../get_json_devices.sh > /dev/null 2>&1
    jq . devices.json | grep id_03 >/dev/null
